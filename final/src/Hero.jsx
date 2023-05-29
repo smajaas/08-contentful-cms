66d9637e81adf343382d9454e67402981fcb5544
smajaas <smajaas@gmail.com>
2023-05-29 23:46:45 +0530

    hero component

---
+++ final/src/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>contentful CMS</h1>
+          <p>
+            YOLO pok pok actually, poke narwhal raclette fashion axe. Austin
+            microdosing sus, thundercats bitters blog kickstarter letterpress
+            kinfolk umami pabst direct trade. Sartorial pop-up grailed twee,
+            freegan waistcoat synth. cronut la croix selvage green juice jawn
+            gorpcore
+          </p>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='woman and the browser' className='img' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ final/src/Projects.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Projects = () => {
+  return <h2>projects</h2>;
+};
+
+export default Projects;
